eabd769184d6816fc6bbd399b80db9afd2a80a2b
tspathiraja <mptspathiraja@gmail.com>
2018-07-01 13:16:05 +0530

    exercise2.4

---
+++ exercise2.4.c
@@ -0,0 +1,12 @@
+/* exercise 2.4 */
+#include <stdio.h>
+int main(){
+	int a=3;
+	float b=10.0;
+	float c;
+	c=b/a;
+	printf("A is %d\n",a);
+	printf("B is %.0f\n",b);
+	printf("Answer is %.2f\n",c);
+	return 0;
+}
