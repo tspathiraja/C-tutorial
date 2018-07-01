9a6ceb0777ab85c4536e6c1293bb923dd1683423
tspathiraja <mptspathiraja@gmail.com>
2018-07-01 13:16:16 +0530

    exercise2.5

---
+++ exercise2.5.c
@@ -0,0 +1,8 @@
+/* exercise 2.5 */
+#include <stdio.h>
+int main(){
+	float a=34.5678;
+	printf("number rounded to the nearest integer %.0f\n",a);
+	printf("number rounded to two decimal %.2f\n",a);
+	return 0;
+}
