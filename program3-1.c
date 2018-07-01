ad045c937830db78e359f69b9e6b11afc3bf215b
tspathiraja <mptspathiraja@gmail.com>
2018-07-01 13:26:27 +0530

    program3-1

---
+++ program3-1.c
@@ -0,0 +1,16 @@
+/* program 3.1 */
+#include <stdio.h>
+int main(){
+	int a,b;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+        scanf("%d",&b);
+
+	printf("\na+b = %d", a+b);
+	printf("\na-b = %d", a-b);
+	printf("\na*b = %d", a*b);
+	printf("\na/b = %d", a/b);
+	printf("\na%%b = %d\n", a%b);
+	return 0;
+}
