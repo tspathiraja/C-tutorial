b03670109b6b9971be32e721c8c11c7a2d6b0407
tspathiraja <mptspathiraja@gmail.com>
2018-07-01 13:25:56 +0530

    program2-9

---
+++ program2-9.c
@@ -0,0 +1,9 @@
+/* Program 2.9 */
+#include <stdio.h>
+int main(){
+	char first_letter;
+	first_letter='A';
+	printf("character %c\n",first_letter); //display character
+	printf("ASCII value %d\n",first_letter);//display ASCII value
+	return 0;
+}
