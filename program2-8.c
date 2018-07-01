972bc4ac1fc6c8c6159fa09fa47b9bc571b40ace
tspathiraja <mptspathiraja@gmail.com>
2018-07-01 13:25:30 +0530

    program2-8

---
+++ program2-8.c
@@ -0,0 +1,12 @@
+/* Program 2.8 */
+#include <stdio.h>
+int main(){
+	int a=3;
+	float b=10.0;
+	float c;
+	c=b/a;
+	printf("A is %d\n",a);
+	printf("B is %.0f\n",b);
+	printf("Answer is %f\n",c);
+	return 0;
+}
