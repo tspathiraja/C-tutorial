671fb59b7d802332ef7f64c57710e59b2bc40879
tspathiraja <mptspathiraja@gmail.com>
2018-07-01 13:16:32 +0530

    exercise2.7

---
+++ exercise2.7.c
@@ -0,0 +1,12 @@
+/* exercise 2.7 */
+#include <stdio.h>
+int main(){
+	float a,b,sum;
+	printf("Enter 1st number : ");
+	scanf("%f",&a);
+	printf("Enter 2nd number : ");
+	scanf("%f",&b);
+	sum=a+b;
+	printf("Answer > %.0f\n",sum);
+	return 0;
+}
