f2a30bc2a7f6fe67423525f3f625f6a7a3b47432
tspathiraja <mptspathiraja@gmail.com>
2018-07-02 11:38:06 +0530

    exercise4.4

---
+++ exercise/exercise4.4.c
@@ -0,0 +1,20 @@
+/* Program 4.5 */
+#include <stdio.h>
+void main(){
+	int marks;
+	printf("Enter marks: ");
+	scanf("%d",&marks);
+	if(marks > 75){
+		printf("Your grade is: A\n");
+	}
+   	if(marks <75){
+		if (marks >=50)
+			printf("Your grade is: B\n");
+		}
+   	if(marks <50){
+		if (marks >=25)
+			printf("Your grade is: C\n");
+		}
+	if(marks <25)
+		printf ("Your grade is: F\n");
+}
