8691620a7903c6bcf27c4c717cefbbed3c36c59f
tspathiraja <mptspathiraja@gmail.com>
2018-07-02 11:18:19 +0530

    program4.1

---
+++ program/program4-1.c
@@ -0,0 +1,11 @@
+/* program 4.1 */
+#include <stdio.h>
+void main(){
+	int marks;
+	printf("enter marks: ");
+	scanf("%d",&marks);
+
+	if (marks >50){
+		printf("You have passed the exam!\n");
+	}
+}
