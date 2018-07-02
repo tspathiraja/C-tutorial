0343591bb7907cf72318fcab2580bd7f5f393f6b
tspathiraja <mptspathiraja@gmail.com>
2018-07-02 11:18:36 +0530

    program4.2

---
+++ program/program4-2.c
@@ -0,0 +1,14 @@
+/* program 4.2 */
+#include <stdio.h>
+void main(){
+	float amount,discount;
+	printf("enter by amount: ");
+	scanf("%f",&amount);
+	if (amount >=1000){
+		discount=amount*0.95; // final amount( added 5% discount)
+		printf("discount: %.2f\n",amount*0.05);
+		printf("final amount: %.2f\n",discount);
+	}
+	
+}
+
