f45222b9b102de6a081e3697f72608caf8a09c9b
tspathiraja <mptspathiraja@gmail.com>
2018-07-03 15:05:30 +0530

    exercise5.6

---
+++ exercise/exercise5.6.c
@@ -0,0 +1,33 @@
+/* exercise 5.6 */
+#include <stdio.h>
+void main(){
+	int a;
+	do{
+	printf("Menu");
+	printf("\n-----------------------------------");
+	printf("\n1 - Microsoft Word");
+	printf("\n2 - Yahoo messenger");
+	printf("\n3 - AutoCAD");
+	printf("\n4 - Java Games");
+	printf("\n-----------------------------------");
+	printf("\nEnter number of your preference: ");
+	scanf("%d",&a);
+	switch (a){
+		case 1:
+			printf("Personal Computer Software\n");
+			break;
+		case 2:
+			printf("Web based Software\n");
+			break;
+		case 3:
+			printf("Scientific Software\n");
+			break;
+		case 4:
+			printf("Embedded Software\n");
+			break;
+		default:
+			printf("Incorrect input\n");
+	}
+	}
+	while (a>0);
+}
